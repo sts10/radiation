7c10141adc745f95196575f6d24c03dd9b88222a
sts10 <sschlinkert@gmail.com>
2014-11-09 17:31:10 -0500

    checks if no timezone given just runs Time.new

--- lib/time.rb
+++ lib/time.rb
@@ -2,15 +2,19 @@ class Time
   
    # tzstring e.g. 'America/Los_Angeles'
 
-   def in_timezone(tzstring)
-     tz = TZInfo::Timezone.get tzstring
-     p = tz.period_for_utc self
-     e = self.localtime(p.utc_offset)
+  def in_timezone(tzstring)
+    if tzstring == ''
+      e = Time.new
+    else
+      tz = TZInfo::Timezone.get tzstring
+      p = tz.period_for_utc self
+      e = self.localtime(p.utc_offset)
 
-     if e.isdst
+      if e.isdst
         e = e + 60*60
-     end
+      end
+    end
      e
-   end
+  end
 
 end
--- user_settings.rb
+++ user_settings.rb
@@ -1,4 +1,7 @@
-$my_timezone = 'America/New_York'
+
+# Set time zone. Examples: 'America/Los_Angeles' or 'America/New York'
+$my_timezone = '' 
+
 $my_text_editor_command = 'vim'
 $my_blog_location = '../public_html/blog.html'
 
