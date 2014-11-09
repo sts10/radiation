4ff5a592b54d5ac3989b9dbbcdf4740308404019
sts10 <sschlinkert@gmail.com>
2014-11-09 15:22:04 -0500

    provides an override to install Radiation for users upgrading to v 0.2.0

--- bin/runner.rb
+++ bin/runner.rb
@@ -4,6 +4,31 @@ require_relative '../config/environment'
 if ARGV[0] == "publish"
   this_blog = Blog.new
   this_blog.publish!('../radiation_posts/')
+  
+elsif ARGV[0] == "install"
+  puts "Installing some Ruby gems..."
+  system "gem install tzinfo"
+  puts "Ruby gems installed!"
+
+  system "mkdir ../radiation_posts"
+  
+  if File.exist?($my_template_location)
+    puts "It looks like you already have a template in place. I'll leave it alone."
+  else
+    puts "Creating radiation_templates folder and putting a sample template in there with the proper file name."
+    system "mkdir ../radiation_templates"
+    system "cp sample_templates/blog.html.erb #{$my_template_location}"
+  end
+
+  puts ""
+  puts ""
+  puts "Radiation is now installed!"
+  puts ""
+  puts ""
+
+  puts "You may want to go to ~/radiation_templates/blog.html.erb to fill in your personal information."
+  puts "See ReadMe for more information. We strongly suggest editing your .bash_profile to be able to call Radiation from anywhere."
+
 else
   if File.exist?('../radiation_posts') && File.exist?('../radiation_templates') && File.exist?($my_template_location)
 
