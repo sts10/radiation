b550a779b37bae2134b1a1086d6337915753c546
sts10 <sschlinkert@gmail.com>
2014-11-09 13:12:36 -0500

    created a separate user_settings.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,7 @@ require 'date'
 require 'tzinfo'
 require 'active_support'
 
+require_relative '../user_settings.rb'
 require_relative '../lib/time'
 require_relative '../lib/post'
 require_relative '../lib/blog'
-
-
-$my_timezone = 'America/New_York'
-$my_template_location = '../radiation_templates/blog.html.erb'
-$my_blog_location = '../public_html/blog.html'
-$my_text_editor_command = 'subl'
---
+++ user_settings.rb
@@ -0,0 +1,4 @@
+$my_timezone = 'America/New_York'
+$my_template_location = '../radiation_templates/blog.html.erb'
+$my_blog_location = '../public_html/blog.html'
+$my_text_editor_command = 'vim'
