3a055c2c0cb7055190d67ae319cf72a4cd399f5e
sts10 <sschlinkert@gmail.com>
2014-11-09 17:39:06 -0500

    added more to timezone comment in user_settings

--- user_settings.rb
+++ user_settings.rb
@@ -1,6 +1,6 @@
 
-# Set time zone. Examples: 'America/Los_Angeles' or 'America/New York'
-# $my_timezone = '' 
+# Set time zone. Examples: 'America/Los_Angeles' or 'America/New York'. May set = to '' if you don't want to set a time-zone
+$my_timezone = '' 
 
 $my_text_editor_command = 'vim'
 $my_blog_location = '../public_html/blog.html'
